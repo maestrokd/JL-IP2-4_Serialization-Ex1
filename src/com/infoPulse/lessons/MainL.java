fa2728f99885ac32d61765b3fd71f3cc4949f283
maestrokd <maestrokd@users.noreply.github.com>
2017-10-15 18:52:00 +0300

    I have just created the new project

--- src/com/infoPulse/lessons/MainL.java
+++ src/com/infoPulse/lessons/MainL.java
@@ -6,45 +6,38 @@ import java.util.ArrayList;
 public class MainL {
 
     public static void main(String[] args) {
-
-        //
-        String line;
+//        String line;
         ArrayList<Payment> payments = new ArrayList<>();
 
-        try (FileInputStream inputStreamLocal = new FileInputStream(
-                ".\\payments_input.txt");
-             InputStreamReader inputStreamReaderLocal = new InputStreamReader(
-                     inputStreamLocal, "CP1251");
-             BufferedReader bufferReaderLocal = new BufferedReader(
-                     inputStreamReaderLocal);) {
 
-
-            // int data = inputStream.read();
-            line = bufferReaderLocal.readLine();
+        // Read data of Payments from file "payments_input.txt"
+        try (FileInputStream inputStreamLocal = new FileInputStream(".\\payments_input.txt");
+             InputStreamReader inputStreamReaderLocal = new InputStreamReader(inputStreamLocal, "CP1251");
+             BufferedReader bufferReaderLocal = new BufferedReader(inputStreamReaderLocal))
+        {
+            String line = bufferReaderLocal.readLine();
             while ((line = bufferReaderLocal.readLine()) != null) {
-//                System.out.println(str);
+                System.out.println(line);
                 String[] partOfLine = line.split("\\|");
                 payments.add(new Payment(partOfLine[0], partOfLine[1], partOfLine[2]));
             }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("FileNotFound (payments_input.txt)");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(payments.size());
 
-        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("payment.ser"));)
+        // Serialization data of Payments to file "payment.ser"
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("payment.ser")))
         {
             out.writeObject(payments);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("FileNotFound (payment.ser)");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
